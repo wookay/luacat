7c2fa786a3147314698166ca8802a385956dde59
WooKyoung Noh <wookay.noh@gmail.com>
2011-07-20 17:29:49 +0530

    log_info string

--- TestCases/test_logger.lua
+++ TestCases/test_logger.lua
@@ -7,13 +7,14 @@ require 'StringExt'
 
 log_info( nil )
 log_info( 123 )
-log_info( "a" )
+log_info( "abc" )
 log_info( {1, 2, 3} )
 log_info( {a = 1, b = 2} )
 log_info( "test 1 : %d", 1 )
+log_info( "test \"abc\" : %s", "abc" )
 log_info( "test nil : %s", to_s(nil) )
 log_info( "test {1} : %s", {1} )
-log_info( "test {a=1} {b=1} : %s %s", {a=1}, {b=1})
+log_info( "test {a=1} {b=2} : %s %s", {a=1}, {b=2})
 log_info( {a=1, b=2, c=3, d=4, e=5, f=6, g=7, h=8, i=9, j=10, k=11} )
 
 
@@ -21,13 +22,14 @@ log_info( {a=1, b=2, c=3, d=4, e=5, f=6, g=7, h=8, i=9, j=10, k=11} )
 
 nil
 123
-"a"
+abc
 {1, 2, 3}
 {a = 1, b = 2}
 test 1 : 1
-test nil : "nil"
+test "abc" : abc
+test nil : nil
 test {1} : {1}
-test {a=1} {b=1} : {a = 1} {b = 1}
+test {a=1} {b=2} : {a = 1} {b = 2}
 { a = 1, 
   b = 2, 
   c = 3, 
--- luacat/Logger.lua
+++ luacat/Logger.lua
@@ -7,11 +7,18 @@ require 'TableExt'
 require 'Exception'
 
 function log_info(one, ...)
+  local fun = function(x)
+    if "string" == type(x) then
+      return x
+    else
+      return inspect(x)
+    end
+  end
   local rest = {...}
   if 0 == #rest then
-    print(inspect(one))
+    print(fun(one))
   else
-    try(function() print(string.format(one, unpack(table.map(rest, function(x) return inspect(x) end)))) end,
+    try(function() print(string.format(one, unpack(table.map(rest, fun)))) end,
     function(exception) print(exception, one, inspect(rest)) end)
   end
 end
