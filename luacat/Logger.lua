4da25fe09a805549f89906c29ea7d4aa3fc6c2dc
WooKyoung Noh <wookay.noh@gmail.com>
2011-09-26 14:14:47 +0530

    Refactored log_info_impl

--- luacat/Logger.lua
+++ luacat/Logger.lua
@@ -15,17 +15,17 @@ if nil == Logger then
   }
 end
 
-local function _extract_filename_line_for_log_info(n, traceback)
+local function _extract_filename_line_for_log_info(traceback)
   return _(_(_(_(_(traceback).
-           split(": in function '")).at(n)).split(': ')[1]).split(LF)[2]).lstrip()
+           split(": in function '")).at(-1)).split(': ')[1]).split(LF)[2]).lstrip()
 end
 
-local function print_line(n, line)
+local function print_line(line)
   print(string.format("%-23s %s", 
-    _extract_filename_line_for_log_info(n, debug.traceback()), line))
+    _extract_filename_line_for_log_info(debug.traceback()), line))
 end
 
-function log_info_impl(n, one, ...)
+function log_info_impl(one, ...)
   local fun = function(x)
     if "string" == type(x) then
       return x
@@ -35,17 +35,17 @@ function log_info_impl(n, one, ...)
   end
   local rest = {...}
   if 0 == #rest then
-    print_line(n, fun(one))
+    print_line(fun(one))
   else
     try(function()
       if 'string' == type(one) and String.include(one, PERCENT) then
-        print_line(n, string.format(one, unpack(Table.map(rest, fun))))
+        print_line(string.format(one, unpack(Table.map(rest, fun))))
       else
         format = Table.join(Table.new(1 + #rest, '%s'), SPACE)
-        print_line(n, string.format(format, fun(one), unpack(Table.map(rest, fun))))
+        print_line(string.format(format, fun(one), unpack(Table.map(rest, fun))))
       end
     end,
-    function(exception) print_line(n, exception, one, inspect(rest)) end)
+    function(exception) print_line(exception, one, inspect(rest)) end)
   end
 end
 
@@ -56,7 +56,7 @@ end
 
 function log_info(one, ...)
   if nil == Logger.log_info then
-    log_info_impl(-1, one, ...)
+    log_info_impl(one, ...)
   else
     Logger.log_info(one, ...)
   end
